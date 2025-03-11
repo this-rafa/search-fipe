455e1cc5ea158d7a34a4d49b3624492fe7fb4f53
this-rafa <diegorafaeldev@outlook.com>
2025-03-11 08:58:44 -0300

    Add: pacote service && Classe generica HttpGetService

--- src/main/java/com/drac/fipe/search_fipe/SearchFipeApplication.java
+++ src/main/java/com/drac/fipe/search_fipe/SearchFipeApplication.java
@@ -1,13 +1,24 @@
 package com.drac.fipe.search_fipe;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.drac.fipe.search_fipe.service.HttpGetService;
+
 @SpringBootApplication
-public class SearchFipeApplication {
+public class SearchFipeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchFipeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println(new HttpGetService()
+				.getCallApi("https://parallelum.com.br/fipe/api/v1/carros/marcas/59/modelos/5940/anos/2014-3"));
+
 	}
 
 }
