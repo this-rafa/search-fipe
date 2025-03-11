e90b9575851bf7d8e91144e365ffdf53026d21ee
this-rafa <diegorafaeldev@outlook.com>
2025-03-11 00:25:34 -0300

    add adicionando pacote DTO e classes de DTO

---
+++ src/main/java/com/drac/fipe/search_fipe/dto/FipeObjectDTO.java
@@ -0,0 +1,11 @@
+/**
+ * Um Objeto de Transferência de Dados (DTO) representando um objeto Fipe.
+ * 
+ * @param codigo O código do objeto Fipe.
+ * @param nome O nome do objeto Fipe.
+ */
+package com.drac.fipe.search_fipe.dto;
+
+public record FipeObjectDTO(String codigo, String nome) {
+
+}
