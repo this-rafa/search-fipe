855a1b98e879d304b41d84caf1639f34c93c9600
this-rafa <diegorafaeldev@outlook.com>
2025-03-10 23:50:02 -0300

    inicialização,  configuração inicial && add Jackson databind

---
+++ src/main/java/com/drac/fipe/search_fipe/SearchFipeApplication.java
@@ -0,0 +1,13 @@
+package com.drac.fipe.search_fipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchFipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drac/fipe/search_fipe/SearchFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drac.fipe.search_fipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
